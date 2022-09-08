239889cf3bbb3938c1611e9ea6fcaea987707006
hicham <hicham.elarfaouy.youcode@gmail.com>
2022-09-08 10:52:33 +0100

    circonférence du cercle

---
+++ challenge_10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+void main(){
+
+    int r;
+
+    printf("Donnez rayon : ");
+    scanf("%d", &r);
+
+    printf(" la distance entre deux points est :  %.2f \n", 2*3.14*r);
+}
